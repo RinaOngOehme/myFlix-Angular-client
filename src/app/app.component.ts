53a11a07b3ced9c40abe92e506adb747e36c8b98
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-11-03 10:05:17 +0400

    Update app.component.ts
    update openUserLoginDialog and import UserLoginFormComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -18,6 +19,11 @@ export class AppComponent {
       width: '480px'
     });
   }
-
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      // Assigning the dialog a width
+      width: '480px'
+    });
+  }
 
 }
