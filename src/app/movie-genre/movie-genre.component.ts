7654b5a88ca673ba46ae65ad163dddd59ea53e6a
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-11-11 09:52:11 +0400

    Update movie-genre.component.ts

--- src/app/movie-genre/movie-genre.component.ts
+++ src/app/movie-genre/movie-genre.component.ts
@@ -2,25 +2,31 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatDialogRef } from "@angular/material/dialog";
 
+
 @Component({
   selector: 'app-movie-genre',
   templateUrl: './movie-genre.component.html',
   styleUrls: ['./movie-genre.component.scss']
 })
 export class MovieGenreComponent implements OnInit {
+  genre: any[] = [];
 
   constructor(
+
     public dialogRef: MatDialogRef<MovieGenreComponent>,
     @Inject(MAT_DIALOG_DATA)
 
-    public genre: {
+    public data: {
       Name: string;
       Description: string;
     }
   ) { }
 
   ngOnInit(): void {
+
   }
+
+
 
   closeDialog(): void {
     this.dialogRef.close();
