ae6a8d38706d0358865c5ef152b4da09bd8d643c
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-11-11 09:53:13 +0400

    update getUser function

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { UserEditComponent } from '../user-edit/user-edit.component';
 import { UserDeleteComponent } from '../user-delete/user-delete.component';
 
@@ -10,30 +12,39 @@ import { UserDeleteComponent } from '../user-delete/user-delete.component';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
-  user = {
-    Username: "",
-    Email: "",
-    Birthday: ""
-  }
+  user: any = {};
+  favorites: any[] = [];
+
 
   constructor(
-    public fetchDataApi: FetchApiDataService,
+    public fetchApiData: FetchApiDataService,
     public dialog: MatDialog,
+    public snackBar: MatSnackBar,
+    public router: Router,
   ) { }
 
   ngOnInit(): void {
-    this.fetchDataApi.getUser(localStorage.getItem("user")).subscribe((results) => {
-      console.log(results);
-      // cut off the unneeded part of the birthday
-      const birthday = results.Birthday.slice(0, 10);
-      //assign the user variable
-      this.user = {
-        Username: results.Username,
-        Email: results.Email,
-        Birthday: results.Birthday,
-      }
-    })
+    this.getUser();
   }
+
+  getUser(): void {
+    let user = localStorage.getItem('username');
+    this.fetchApiData.getUser(user).subscribe((res: any) => {
+      this.user = res;
+    });
+  }
+
+  //this.fetchApiData.getUser(localStorage.getItem('username')).subscribe((results) => {
+  // console.log(results);
+  // cut off the unneeded part of the birthday
+  // const birthday = results.Birthday.slice(0, 10);
+  //assign the user variable
+  //this.user = {
+  // User: results.Username,
+  // Email: results.Email,
+  // Birthday: results.Birthday,
+  //}
+  //})
   /**
     * Opens dialog used to edit user information
     */
@@ -52,6 +63,21 @@ export class UserProfileComponent implements OnInit {
     });
   }
 
+  getFavorites(): void {
+
+    this.fetchApiData.getUser(this.user).subscribe((resp: any) => {
+      //get favorites to the favorites array
+      this.favorites = resp.Favorites;
+      return this.favorites;
+    })
+  }
+
+
+  cancel(): void {
+    this.router.navigate(['/profile']).then(() => {
+      window.location.reload();
+    });
+  }
 
 
 }
